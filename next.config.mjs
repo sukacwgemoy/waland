42f3b3998873cdfac1e82d484eec1e045af8ef2f
sukacwgemoy <techgibah@gmail.com>
2025-11-16 20:44:37 +0700

    Initial commit - CS AI Landing Page

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { build } from "velite";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
     return config;
